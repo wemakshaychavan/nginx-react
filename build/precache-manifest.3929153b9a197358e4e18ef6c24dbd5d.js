d9de53641c02eb5dc618397720b003753a64f388
Abhinandan Satpute <satputea@gene.com>
2020-04-15 17:08:19 +0530

    initial commit

---
+++ build/precache-manifest.3929153b9a197358e4e18ef6c24dbd5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f40d7b046cfb5e1132f28a7b5f15c9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92e9c146e8522726c27c",
+    "url": "./static/css/2.d61ac1f4.chunk.css"
+  },
+  {
+    "revision": "f9b274d58d330da63b47",
+    "url": "./static/css/main.c2180cba.chunk.css"
+  },
+  {
+    "revision": "92e9c146e8522726c27c",
+    "url": "./static/js/2.98fa54bc.chunk.js"
+  },
+  {
+    "revision": "4b2e529ed1ad15160be2a079c000d0e7",
+    "url": "./static/js/2.98fa54bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b274d58d330da63b47",
+    "url": "./static/js/main.5b6635be.chunk.js"
+  },
+  {
+    "revision": "bec41a16270d56e35ae7",
+    "url": "./static/js/runtime-main.c8a92e02.js"
+  },
+  {
+    "revision": "b7edbdb578884125badc1104ee079e63",
+    "url": "./static/media/Stripe_Logo.b7edbdb5.png"
+  },
+  {
+    "revision": "dd93d88a6954e46fbed55e21c0941340",
+    "url": "./static/media/background.dd93d88a.jpg"
+  },
+  {
+    "revision": "ddfe39b224038e74c8247b5f9aaeb6b2",
+    "url": "./static/media/logo.ddfe39b2.png"
+  }
+]);
