8d1cb49915f04bc98f16fbdec1a0251675f70a98
Abhinandan Satpute <satputea@gene.com>
2020-04-16 00:09:17 +0530

    updated build

---
+++ build/precache-manifest.05c8faa25ac1b8826c5c755b20a13679.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558f1f9b405766ea47f749a788d414e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16be0439b890d178b942",
+    "url": "/static/css/2.d61ac1f4.chunk.css"
+  },
+  {
+    "revision": "4ce367e7d84e50611ed9",
+    "url": "/static/css/main.ed2d2fd5.chunk.css"
+  },
+  {
+    "revision": "16be0439b890d178b942",
+    "url": "/static/js/2.06cd9f1e.chunk.js"
+  },
+  {
+    "revision": "4b2e529ed1ad15160be2a079c000d0e7",
+    "url": "/static/js/2.06cd9f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce367e7d84e50611ed9",
+    "url": "/static/js/main.0ec2c612.chunk.js"
+  },
+  {
+    "revision": "64951cb9ee38b7ef2171",
+    "url": "/static/js/runtime-main.16206e93.js"
+  },
+  {
+    "revision": "b7edbdb578884125badc1104ee079e63",
+    "url": "/static/media/Stripe_Logo.b7edbdb5.png"
+  },
+  {
+    "revision": "dd93d88a6954e46fbed55e21c0941340",
+    "url": "/static/media/background.dd93d88a.jpg"
+  },
+  {
+    "revision": "ddfe39b224038e74c8247b5f9aaeb6b2",
+    "url": "/static/media/logo.ddfe39b2.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3929153b9a197358e4e18ef6c24dbd5d.js"
+  "/precache-manifest.05c8faa25ac1b8826c5c755b20a13679.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
