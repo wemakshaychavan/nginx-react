91882837871a0d9b99c43318f987f8aba13dc224
Abhinandan Satpute <satputea@gene.com>
2020-04-16 22:46:12 +0530

    build updated

---
+++ build/precache-manifest.3e718141d62ba9787347805d54f57510.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e8a08f7c9bb4a5e7ee786c1c0699e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f685bdfa0903938ff4",
+    "url": "/static/css/2.d61ac1f4.chunk.css"
+  },
+  {
+    "revision": "306847bcaefcf50320cd",
+    "url": "/static/css/main.ed2d2fd5.chunk.css"
+  },
+  {
+    "revision": "14f685bdfa0903938ff4",
+    "url": "/static/js/2.a5875015.chunk.js"
+  },
+  {
+    "revision": "4b2e529ed1ad15160be2a079c000d0e7",
+    "url": "/static/js/2.a5875015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306847bcaefcf50320cd",
+    "url": "/static/js/main.ed5e3885.chunk.js"
+  },
+  {
+    "revision": "64951cb9ee38b7ef2171",
+    "url": "/static/js/runtime-main.16206e93.js"
+  },
+  {
+    "revision": "b7edbdb578884125badc1104ee079e63",
+    "url": "/static/media/Stripe_Logo.b7edbdb5.png"
+  },
+  {
+    "revision": "dd93d88a6954e46fbed55e21c0941340",
+    "url": "/static/media/background.dd93d88a.jpg"
+  },
+  {
+    "revision": "ddfe39b224038e74c8247b5f9aaeb6b2",
+    "url": "/static/media/logo.ddfe39b2.png"
+  }
+]);
