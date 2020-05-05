1482be142d9291a2714b36d7bc5dc1883fe98aed
akshay <devasakshay@gmail.com>
2020-05-05 22:23:30 +0530

    build: 2020/05/05

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf01898594be8f2e93dbce1871119ab3.js"
+  "/precache-manifest.de14ea8851554238a606dba4f4eee40c.js"
 );
 
 self.addEventListener('message', (event) => {
