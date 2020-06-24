ab75b5bf73b414b90a897af7152e861bc4a90f90
akshay <devasakshay@gmail.com>
2020-06-24 15:18:33 +0530

    digi dev

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0eafec1c56caf3697b7c08b7ddf35979.js"
+  "/precache-manifest.ce7997ae9776a6a14a208f1156df7ce8.js"
 );
 
 self.addEventListener('message', (event) => {
